a7b9c1f121c16e5d6248f08967d5f888a183b7fd
Nada Elshahawi <76527313+NadaMohamedMedhat@users.noreply.github.com>
2024-06-01 03:53:26 +0300

    -starting in ahadeth tab - adding alert dialog of localization

--- lib/presentation/home/tabs/ahadeth/ahadeth_tab.dart
+++ lib/presentation/home/tabs/ahadeth/ahadeth_tab.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AhadethTab extends StatelessWidget {
   const AhadethTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Image.asset('assets/images/hadth_image.png'),
+          ),
+          SizedBox(
+            height: 5.h,
+          ),
+          Divider(
+            color: Theme.of(context).colorScheme.primary,
+            thickness: 5.h,
+          ),
+          Text(
+            'Ahadeth',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          Divider(
+            color: Theme.of(context).colorScheme.primary,
+            thickness: 5.h,
+          ),
+        ],
+      ),
     );
+  }
+
+  readAhadethFiles(int index) async {
+    String ahadethContent =
+        await rootBundle.loadString('assets/files/ahadeth.txt');
   }
 }
